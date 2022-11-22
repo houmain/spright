7dbe89d9dcfc13960d74a9807c5d41ad57c2b616
houmain <houmain@posteo.net>
2022-11-22 18:11:56 +0100

    Removed support for reading JPEG, PSD, HDR

--- libs/stb/stb_impl.cpp
+++ libs/stb/stb_impl.cpp
@@ -7,6 +7,11 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_NO_FAILURE_STRINGS
+#define STBI_NO_JPEG
+#define STBI_NO_PSD
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM 
 #include "stb_image.h"
 
 
