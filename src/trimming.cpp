713067f7a744608529d917e2fae34b7a8bb655b9
houmain <houmain@posteo.net>
2023-05-28 20:47:56 +0200

    Fixed trim none

--- src/trimming.cpp
+++ src/trimming.cpp
@@ -134,17 +134,18 @@ namespace {
   }
 
   void trim_sprite(Sprite& sprite) {
-    if (sprite.trim == Trim::none) {
-      sprite.trimmed_source_rect = sprite.source_rect;
-      return;
+    
+    if (sprite.trim != Trim::none) {
+      sprite.trimmed_source_rect = get_used_bounds(*sprite.source,
+        sprite.trim_gray_levels, sprite.trim_threshold, sprite.source_rect);
+  
+      if (sprite.trim_margin)
+        sprite.trimmed_source_rect = intersect(expand(
+          sprite.trimmed_source_rect, sprite.trim_margin), sprite.source_rect);
     }
-
-    sprite.trimmed_source_rect = get_used_bounds(*sprite.source,
-      sprite.trim_gray_levels, sprite.trim_threshold, sprite.source_rect);
-
-    if (sprite.trim_margin)
-      sprite.trimmed_source_rect = intersect(expand(
-        sprite.trimmed_source_rect, sprite.trim_margin), sprite.source_rect);
+    else {
+      sprite.trimmed_source_rect = sprite.source_rect;
+    }
 
     if (sprite.trim == Trim::convex) {
       const auto levels = (sprite.trim_gray_levels ?
